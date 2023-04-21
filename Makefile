15d017c58f8ffa4fa197aeeb175d91078a521bc4
ecerami <cerami@gmail.com>
2023-04-21 15:02:08 -0400

    Added gcp example

--- Makefile
+++ Makefile
@@ -6,6 +6,9 @@ deploy_traditional:
 deploy_task_flow:
 	cp dags/task_flow/*.py $(AIRFLOW_HOME)/dags
 
+deploy_gcp:
+	cp dags/gcp/*.py $(AIRFLOW_HOME)/dags
+
 deploy_other:
 	cp dags/mysql/*.py $(AIRFLOW_HOME)/dags
 	cp dags/expand/*.py $(AIRFLOW_HOME)/dags
---
+++ dags/gcp/gcs.py
@@ -0,0 +1,40 @@
+"""
+Connect to Google Cloud Storage (GCS) Bucket.
+"""
+from airflow.decorators import dag, task
+from datetime import datetime
+import logging
+from airflow.providers.google.cloud.operators.gcs import GCSListObjectsOperator
+
+logger = logging.getLogger("airflow.task")
+
+# Create the DAG
+@dag(
+    dag_id="gcs_list",
+    start_date=datetime(2023,1,1),
+    schedule=None,
+    catchup=False,
+    tags= ["gcp"],
+)
+def bucket_list():
+
+    gcp_file_list = GCSListObjectsOperator(
+        task_id='gcs_list_files',
+        bucket='hdash',
+        gcp_conn_id="gcp"
+    )
+
+    @task()
+    def output_file_list(file_list):
+        """Output File List to the Log."""
+        logger.info("Verifying Google Connection")
+        logger.info(f"Total number of files:  {len(file_list)}")
+        for file in file_list:
+            logger.info(file)
+
+    # Call the task functions
+    output_file_list(gcp_file_list.output)
+
+
+# Call the dag function to register the DAG
+bucket_list()
